74c50942bf98aa81cbfa6e61e84306909e7322ba
Jhair <jhairzp27@hotmail.com>
2024-07-29 15:34:25 -0500

    Agrego funcionalidad de descarga y control de usuarios

--- Bot/run_application.py
+++ Bot/run_application.py
@@ -7,13 +7,7 @@ APPLICATION_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Des
 
 def run_application():
     if os.path.exists(APPLICATION_PATH):
-        try:
-            os.startfile(APPLICATION_PATH)  # En Windows, para abrir un archivo o ejecutar un programa
-            print(f"La aplicación {APPLICATION_PATH} se ha iniciado.")
-        except Exception as e:
-            print(f"No se pudo iniciar la aplicación: {str(e)}")
-    else:
-        print(f"El archivo {APPLICATION_PATH} no existe.")
-
+        os.startfile(APPLICATION_PATH)  # En Windows, para abrir un archivo o ejecutar un programa
+            
 if __name__ == "__main__":
     run_application()
--- Database/datab.py
+++ Database/datab.py
@@ -14,17 +14,33 @@ def create_table_if_not_exists():
     try:
         conex = sqlite3.connect(DB_PATH)
         cursor = conex.cursor()
+        
+        # Crear tabla de usuarios
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 telegram_id INTEGER PRIMARY KEY,
                 name TEXT NOT NULL
             )
         ''')
+        
+        # Crear tabla de capturas de pantalla
+        cursor.execute('''
+           
+            CREATE TABLE IF NOT EXISTS screenshots (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id INTEGER,
+                screenshot_path TEXT,
+                timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (user_id) REFERENCES users (telegram_id)
+            )
+        ''')
+        
         conex.commit()
         cursor.close()
         conex.close()
     except sqlite3.OperationalError as e:
-        print(f"Error al crear la tabla en la base de datos: {str(e)}")
+        print(f"Error al crear las tablas en la base de datos: {str(e)}")
+
 
 def insert_or_update_user(telegram_id: int, name: str):
     try:
@@ -39,6 +55,21 @@ def insert_or_update_user(telegram_id: int, name: str):
         conex.close()
     except sqlite3.OperationalError as e:
         print(f"Error al insertar o actualizar el usuario en la base de datos: {str(e)}")
+        
+def insert_screenshot(user_id: int, screenshot_path: str):
+    try:
+        conex = sqlite3.connect(DB_PATH)
+        cursor = conex.cursor()
+        cursor.execute('''
+            INSERT INTO screenshots (user_id, screenshot_path)
+            VALUES (?, ?)
+        ''', (user_id, screenshot_path))
+        conex.commit()
+        cursor.close()
+        conex.close()
+    except sqlite3.OperationalError as e:
+        print(f"Error al insertar la captura de pantalla en la base de datos: {str(e)}")
+
 
 # Llamar a la función para asegurarse de que la tabla exista
 create_table_if_not_exists()
